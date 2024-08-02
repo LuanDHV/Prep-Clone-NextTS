e56d94a665652ba3c5591279dcfef0a9ffe2c093
LuanDHV <luandhv1406@gmail.com>
2024-08-02 09:00:45 +0700

    update structure file

--- frontend/src/app/learning-dashboard/layout.tsx
+++ frontend/src/app/learning-dashboard/layout.tsx
@@ -1,4 +1,5 @@
-import HeaderLearningDashboard from "@/components/layouts/HeaderLearningDashboard";
+import HeaderLearningDashboard from "@/components/layouts/LearningDashboard/HeaderLearningDashboard";
+import NavbarLearningDashboard from "@/components/layouts/LearningDashboard/NavbarLearningDashboard";
 
 export default function LearningDashboardLayout({
   children,
@@ -10,7 +11,7 @@ export default function LearningDashboardLayout({
       <HeaderLearningDashboard />
       <div className="mt-[66px] flex flex-1">
         <div className="fixed bottom-0 top-[66px] w-1/6 border-r-[1px] border-neutral-200 p-4">
-          <h2>NavBar</h2>
+          <NavbarLearningDashboard />
         </div>
         <main className="ml-[16.67%] w-5/6 p-8">{children}</main>
       </div>
---
+++ frontend/src/components/layouts/LearningDashboard/NavbarLearningDashboard.tsx
@@ -0,0 +1,7 @@
+export default function NavbarLearningDashboard() {
+  return (
+    <>
+      <h2>NavBar</h2>
+    </>
+  );
+}
