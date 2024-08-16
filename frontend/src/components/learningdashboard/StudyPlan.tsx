ebb3219b8d26055f3591ff0bb3ee34d27340240a
LuanDHV <luandhv1406@gmail.com>
2024-08-16 19:31:57 +0700

    fix ui study plan page

--- frontend/src/components/learningdashboard/StudyPlan.tsx
+++ frontend/src/components/learningdashboard/StudyPlan.tsx
@@ -52,17 +52,16 @@ export default function StudyPlant() {
         />
       </div>
 
+      {/* Show Information Course */}
       {course ? (
-        // Show lessons list
         <div className="absolute left-1/2 top-10 w-5/6 -translate-x-1/2 transform md:w-4/6">
           <div className="flex flex-col justify-between gap-10 xl:flex-row">
-            <div className="w-full xl:w-3/6">
+            <div className="w-full xl:w-4/6">
               <div className="flex h-auto w-full flex-col items-center justify-center gap-2 rounded-3xl bg-white p-5 shadow-xl xl:gap-3 xl:py-7">
                 <h1 className="text-3xl font-extrabold text-blue-500">
                   {course.courseType} - {course.name}
                 </h1>
-                <p className="font-bold">{course.title}</p>
-                <p className="text-gray-700">{course.description}</p>
+                <p className="font-bold">{course.title}</p>b
                 <div className="flex w-full justify-center gap-7">
                   <div className="flex items-center gap-2">
                     <Image
@@ -86,7 +85,9 @@ export default function StudyPlant() {
                   </div>
                 </div>
               </div>
-              <div className="mt-5 hidden h-auto w-full items-center justify-center rounded-3xl bg-white p-5 xl:block">
+
+              {/* Show List Lessons Left */}
+              <div className="mt-5 hidden h-auto w-full items-center justify-center rounded-3xl bg-white p-8 xl:block">
                 {lessons.length > 0 ? (
                   <div className="flex flex-wrap justify-center">
                     {lessons.map((lesson: ILessons, index: number) => (
@@ -98,8 +99,8 @@ export default function StudyPlant() {
                           <Image
                             src="/imgs/learningdashboard/hx-b2.svg"
                             alt="hx-b2"
-                            width={40}
-                            height={40}
+                            width={36}
+                            height={36}
                             className="object-cover"
                           />
                           <p className="absolute inset-0 flex items-center justify-center font-bold text-white">
@@ -112,7 +113,7 @@ export default function StudyPlant() {
                 ) : (
                   <p>Không có bài học nào để hiển thị.</p>
                 )}
-                <div className="mt-5 flex items-center justify-between">
+                <div className="mx-auto mt-5 flex items-center justify-between">
                   <div className="grid grid-rows-2">
                     <div className="flex items-center">
                       <Image
@@ -152,6 +153,7 @@ export default function StudyPlant() {
               </div>
             </div>
 
+            {/* Show List Lessons Right */}
             <div className="custom-scrollbar flex h-[330px] w-full justify-center overflow-y-scroll md:h-[620px] xl:w-2/6">
               <div className="w-5/6">
                 {lessons.length > 0 ? (
